2e6239052e6fb46ffc4959102f13da79140e6380
Pol Major <44375854+polmajor@users.noreply.github.com>
2019-05-07 12:46:16 +0200

    Add files via upload

--- script.py
+++ script.py
@@ -1,45 +1,15 @@
 import json
 import io
-from flask import Flask, request, redirect
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
-from flask_login import LoginManager, current_user, login_user, logout_user, login_required, UserMixin
 from serve import predict_bayes
 
 app = Flask(__name__)
 cors = CORS(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-app.config['SECRET_KEY'] = "pertussis"
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User(user_id)
-
-class User(UserMixin):
-    def __init__(self,id):
-        self.id = id
-    
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    f=io.open("login.html", 'r')
-    html = f.read()
-    return html
-
-@app.route('/success', methods=['POST'])
-def success():    
-    ida = request.text
-    if (ida=="pertussis"):
-        login_user(User(1))
-        return redirect("https://pmajortfm.herokuapp.com/bayesian", code=200)
-    else:
-        return redirect("https://pmajortfm.herokuapp.com/login", code=302)
 
 
 #Define the post method.
 @app.route('/bayesian', methods=['POST'])
-@login_required
 def bPredict():
     """ 
     Takes in a json file, then returns the conditional probabilites
