38426b3f66098934ad1699cf10f94867127b4bc1
Pol Major <44375854+polmajor@users.noreply.github.com>
2019-05-07 12:08:21 +0200

    Add files via upload

--- script.py
+++ script.py
@@ -2,13 +2,29 @@ import json
 import io
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required, UserMixin
 from serve import predict_bayes
 
 app = Flask(__name__)
 cors = CORS(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+app.config['PW'] = "pertussis"
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+class User(UserMixin):
+    def __init__(self,id):
+        self.id = id
+    
+    
 #Define the post method.
 @app.route('/bayesian', methods=['POST'])
+@login_required
 def bPredict():
     """ 
     Takes in a json file, then returns the conditional probabilites
