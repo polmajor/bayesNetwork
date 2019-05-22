3c3e6dfb9c031d41610ac275ca3f1f465d47cdaf
Pol Major <44375854+polmajor@users.noreply.github.com>
2019-05-22 17:07:38 +0200

    Add files via upload

--- serve.py
+++ serve.py
@@ -19,48 +19,22 @@ def predict_bayes(idata):
     fim3 = df.columns[8:10]
     period = df.columns[10:13]
 
-    dropped = []
-    to_predict = []
-    corder = df.copy()
 
-    if (idata[1] != 0):
-        df[clade[idata[1]-1]] = 1
-        corder.drop(clade, axis=1, inplace=True)
-    else:
-        df.drop(clade, axis=1, inplace=True)
-        dropped.append(clade)
+    to_predict = []
+
+    if (idata[1] == 0):
         to_predict.append("Clade")
 
-    if (idata[0] != 0):
-        df[period[idata[0]-1]] = 1
-        corder.drop(period, axis=1, inplace=True)
-    else:
-        df.drop(period, axis=1, inplace=True)
-        dropped.append(period)
+    if (idata[0] == 0):
         to_predict.append("DATE")   
 
-    if (idata[4] != 0):
-        df[fim3[idata[4]-1]] = 1
-        corder.drop(fim3, axis=1, inplace=True)
-    else:
-        df.drop(fim3, axis=1, inplace=True)
-        dropped.append(fim3)
+    if (idata[4] == 0):
         to_predict.append("fim3 allele")
 
-    if (idata[3] != 0):
-        df[prn[idata[3]-1]] = 1
-        corder.drop(prn, axis=1, inplace=True)
-    else:
-        df.drop(prn, axis=1, inplace=True)
-        dropped.append(prn)
+    if (idata[3] == 0):
         to_predict.append("prn allele")    
 
-    if (idata[2] != 0):
-        df[ptxP[idata[2]-1]] = 1
-        corder.drop(ptxP, axis=1, inplace=True)
-    else:
-        df.drop(ptxP, axis=1, inplace=True)
-        dropped.append(ptxP)
+    if (idata[2] == 0):
         to_predict.append("ptxP allele")
 
     idf = pd.DataFrame([idata], columns=["DATE","Clade","ptxP allele","prn allele","fim3 allele"])
@@ -71,21 +45,36 @@ def predict_bayes(idata):
     model = pickle.load(open("bayesian_net.pickle.dat", "rb"))
     p = model.predict_probability(idf)
     
-    columns = []
-    for dr in dropped:
-        for d in dr:
-            columns.append(d)
-
-    vals = [str(np.round(v*100,2))+"%" for v in p.values[0]]
-    final_df = pd.DataFrame([vals], columns=corder.columns)
-    
+    c_names = []
     best = []
 
-    for dr in dropped:
-        b = np.argmax(final_df[dr].values[0])
-        best.append(dr[b])
-
-    ht = final_df[corder.columns].to_html(na_rep = "", index = False).replace('\n','')
+    for tp in to_predict:
+        if tp == 'Clade':
+            p = p.rename({"Clade_0":"Clade_(1,2)","Clade_1": "Clade_3","Clade_2": "Clade_4"}, axis=1)
+            b = np.argmax(p[clade].values[0])
+            best.append(clade[b])
+        if tp == 'DATE':
+            p = p.rename({"DATE_0":"Period_<1998, WCV","DATE_1": "Period_1998-2010, ACV-P1","DATE_2": "Period_>2010, ACV-P2"}, axis=1)
+            b = np.argmax(p[period].values[0])
+            best.append(period[b])
+        if tp == 'fim3 allele':
+            p = p.rename({"fim3 allele_1":"fim3 allele_2","fim3 allele_0": "fim3 allele_1"}, axis=1)
+            b = np.argmax(p[fim3].values[0])
+            best.append(fim3[b])
+        if tp == 'prn allele':
+            p = p.rename({"prn allele_2":"prn allele_3","prn allele_1": "prn allele_2","prn allele_0": "prn allele_1"}, axis=1)
+            b = np.argmax(p[prn].values[0])
+            best.append(prn[b])
+        if tp == 'ptxP allele':
+            p = p.rename({"ptxP allele_1":"ptxP allele_3","ptxP allele_0": "ptxP allele_1"}, axis=1)
+            b = np.argmax(p[ptxP].values[0])
+            best.append(ptxP[b])
+            
+    vals = [str(np.round(v*100,2))+"%" for v in p.values[0]]
+    final_df = pd.DataFrame([vals], columns=p.columns)
+    final_df
+    
+    ht = p.to_html(na_rep = "", index = False).replace('\n','')
     ht = ht.replace('<table border="1" class="dataframe">', '<table class="table table-bordered" id="myTable2">')
     for b in best:
         c=b
