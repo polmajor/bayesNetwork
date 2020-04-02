01ccc8868938b4fbe4f5d12a925d89e22d2da383
Pol Major <44375854+polmajor@users.noreply.github.com>
2020-04-02 19:01:07 +0200

    Add files via upload

--- script.py
+++ script.py
@@ -7,26 +7,6 @@ from serve import predict_bayes
 app = Flask(__name__)
 cors = CORS(app)
 
-#Define the post method.
-@app.route('/mail', methods=['POST'])
-def send():
-    """ 
-    Takes in a json file, then sends the email.
-    """
-    
-    # Data the user input
-    input_data = request.json
-    l = input_data["login"]
-    p = input_data["pass"]
-    f = input_data["from"]
-    t = input_data["to"]
-    m = input_data["message"]
-    
-    #API function
-    conn = initialize(l,p)
-    response = send_email(conn,f,t,m)
-
-    return response
 
 #Define the post method.
 @app.route('/bayesian', methods=['POST'])
